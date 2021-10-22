b9b8ff68d41167f42db5e56128eee0a7210d3955
Emmanuel Izilov <em5750@gmail.com>
2021-10-22 17:06:05 -0400

    added expect methods

--- qa/tictactoe.test.ts
+++ qa/tictactoe.test.ts
@@ -22,23 +22,27 @@ test('I can start a game', async () => {
 test('Clicking upper left square should add X to the square', async () => {
     let square = await driver.findElement(By.id('cell-0'));
     await square.click()
+
+    expect(await square.getText()).toContain('X')
     await driver.sleep(2000)
 });
 
-// test('Computer makes a move after I do', async () => {
-//     let uppMidSqr = await driver.findElement(By.id('cell-1'));
-//     expect(uppMidSqr).toContain('O')
-//     await driver.sleep(2000)
-// })
+test('Computer makes a move after I do', async () => {
+    let uppMidSqr = await driver.findElement(By.id('cell-1'));
+    expect(await uppMidSqr.getText()).toContain('O')
+    await driver.sleep(2000)
+})
 
 test('Clicking upper right square should add X to the square', async () => {
     let square = await driver.findElement(By.id('cell-2'));
     await square.click()
+    expect(await square.getText()).toContain('X')
     await driver.sleep(2000)
 });
 
 test('Clicking lower right square should add X to the square', async () => {
     let square = await driver.findElement(By.id('cell-8'));
     await square.click()
+    expect(await square.getText()).toContain('X')
     await driver.sleep(2000)
 });
